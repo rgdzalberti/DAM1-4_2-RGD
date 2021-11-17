f978e6a0eaf710c10a807dbf5f94232ca33d3f7e
Ricardo Gallego Domínguez <rgaldom614@g.educaand.es>
2021-11-17 21:34:46 +0100

    dth

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package un4.collections
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+data class Tienda(val nombre: String, val clientes: List<Clientes>)
+{
+    fun Tienda.obtenerConjuntoDeClientes(): Set<Clientes> {
+        return clientes.toSet()
+    }
+
+    fun Tienda.obtenerCiudadesDeClientes(): Set<Ciudad>
+    {
+        val ciudades = mutableListOf<Ciudad>()
+        clientes.forEach {ciudades.add(it.ciudad)}
+        return ciudades.toSet()
+    }
+
+
+
+}
+
+data class Clientes(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
+    override fun toString() = "$nombre from ${ciudad.nombre}"
+}
+
+data class Pedido(val productos: List<Producto>, val estaEntregado: Boolean)
+
+data class Producto(val nombre: String, val precio: Double) {
+    override fun toString() = "'$nombre' for $precio"
+}
+
+data class Ciudad(val nombre: String) {
+    override fun toString() = nombre
+}
+
+fun main() {
+
 }
