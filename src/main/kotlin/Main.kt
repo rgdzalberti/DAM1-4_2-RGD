c0106b73eb7d4e375ad24f8f6d72186c82b061e3
Ricardo Gallego Domínguez <rgaldom614@g.educaand.es>
2021-11-19 20:53:22 +0100

    +

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,51 +2,80 @@ package un4.collections
 
 data class Tienda(val nombre: String, val clientes: List<Clientes>)
 {
-    fun Tienda.obtenerConjuntoDeClientes(): Set<Clientes> {
+    fun obtenerConjuntoDeClientes(): Set<Clientes> {
         return clientes.toSet()
     }
 
-    fun Tienda.obtenerCiudadesDeClientes(): Set<Ciudad>
+    fun obtenerCiudadesDeClientes(): Set<Ciudad>
     {
         val ciudades = mutableListOf<Ciudad>()
         clientes.forEach {ciudades.add(it.ciudad)}
         return ciudades.toSet()
     }
-
-    fun Tienda.obtenerClientesPorCiudad(Ciudad:Ciudad): List<Clientes>
+    fun obtenerClientesPorCiudad(Ciudad:Ciudad): List<Clientes>
     {
         val clientesPorCiudad = mutableListOf<Clientes>()
         clientes.forEach {if (Ciudad == it.ciudad){clientesPorCiudad.add(it)}}
         return clientesPorCiudad
     }
-
-    fun Tienda.checkTodosClientesSonDe(ciudad : Ciudad): Boolean {
+    fun checkTodosClientesSonDe(ciudad : Ciudad): Boolean {
         return clientes.all {it.ciudad == ciudad}
     }
-
-    fun Tienda.hayClientesDe(ciudad: Ciudad): Boolean {
+    fun hayClientesDe(ciudad: Ciudad): Boolean {
         return clientes.any {it.ciudad == ciudad}
     }
-
-    fun Tienda.cuentaClientesDe(ciudad: Ciudad): Int {
+    fun cuentaClientesDe(ciudad: Ciudad): Int {
         return clientes.count {it.ciudad == ciudad}
     }
-
-    fun Tienda.encuentraClienteDe(ciudad: Ciudad): Clientes? {
+    fun encuentraClienteDe(ciudad: Ciudad): Clientes? {
         return clientes.find {it.ciudad == ciudad}
     }
-
-    fun Tienda.obtenerClientesOrdenadosPorPedidos(): List<Clientes> {
+    fun obtenerClientesOrdenadosPorPedidos(): List<Clientes> {
         return clientes.sortedBy {it.pedidos.size}
     }
-    fun Tienda.obtenerClientesConPedidosSinEntregar(): Set<Clientes> {
-        return clientes.filter { it -> it.pedidos.any { !it.estaEntregado }}.toSet()
+    fun obtenerClientesConPedidosSinEntregar(): Set<Clientes> {
+        return clientes.filter {it.pedidos.any { !it.estaEntregado }}.toSet()
     }
+
+    fun obtenerProductosPedidos(): Set<Producto> {
+        return clientes.flatMap {it.pedidos.flatMap {it.productos}}.toSet()
+    }
+
+    fun obtenerNumeroVecesProductoPedido(producto: Producto): Int {
+        var NumeroVecesProductoPedido: Int = 0
+        clientes.forEach {it.pedidos.forEach {it.productos.forEach {if (it==producto) {NumeroVecesProductoPedido++} } } }
+        return NumeroVecesProductoPedido
+    }
+
+    fun agrupaClientesPorCiudad(): Map<Ciudad, List<Clientes>> {
+
+    }
+
+
 
 }
 
 data class Clientes(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
     override fun toString() = "$nombre from ${ciudad.nombre}"
+
+    fun obtenerProductosPedidos(): List<Producto> {
+        return pedidos.flatMap {it.productos.toList()}
+    }
+
+    fun encuentraProductoMasCaro(): Producto?
+    {
+        //var juja = pedidos.filter {it.productos.find {  }}
+        //var juja = pedidos.filter {it.estaEntregado}
+        return juja
+    }
+
+    fun dineroGastado(): Double {
+        var arrayNumeros = pedidos.flatMap {it.productos.filter}
+        //arrayNumeros.sum()
+        return arrayNumeros
+    }
+
+
 }
 
 data class Pedido(val productos: List<Producto>, val estaEntregado: Boolean)
